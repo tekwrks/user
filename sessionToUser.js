8a13d2829724d02575a9cd2722672b90f6d9c52b
WhoMeNope <whomenope@outlook.com>
2018-11-25 16:15:04 +0100

    tests

--- sessionToUser.js
+++ sessionToUser.js
@@ -7,27 +7,28 @@ module.exports = (User) => function (req, res, next) {
     : null
 
   if (id !== null) {
-    req.userID = new mongoose.Types.ObjectId(`${id}`)
-
-    if (!req.userID.isValid) {
-      logger.error(`invalid userID : ${req.userID}`)
+    const idText = `${id}`
+    if (!mongoose.Types.ObjectId.isValid(idText)) {
+      logger.error(`invalid userID : ${idText}`)
       req.user = null
       next()
     }
+    else {
+      req.userID = new mongoose.Types.ObjectId(idText)
 
-    User.findById(req.userID, function (err, user) {
-      if (err) {
-        logger.debug(err)
-        req.user = null
-      }
-      else {
-        req.user = user
-      }
+      User.findById(req.userID, function (err, user) {
+        if (err) {
+          logger.debug(err)
+          req.user = null
+        }
+        else {
+          logger.debug(`got user : ${user.displayName || null}`)
+          req.user = user
+        }
 
-      logger.debug(`got user : ${user.displayName || null}`)
-
-      next()
-    })
+        next()
+      })
+    }
   }
   else {
     logger.error(`no session found for sessionID : ${req.sessionID}`)
---
+++ sessionToUser.test.js
@@ -0,0 +1,91 @@
+/* eslint-disable */
+
+// user db stub
+const UserStub = {
+  user: { displayName: "name" },
+  findById: function (id, cb) {
+    if (this.succeed)
+      cb(null, this.user)
+    else
+      cb("error", null)
+  },
+  succeed: true,
+}
+
+// require module
+let m = null
+beforeAll(() => {
+  m = require('./sessionToUser')(UserStub)
+  return m
+})
+
+test('requires module and sets it up', () => {
+  expect(m).toBeTruthy()
+})
+
+test('sends 401 for no userID found', done => {
+  const req = {
+    sessionID: "sessionID", // for logger
+  }
+  const res = {
+    status: function (code) {
+      if(code === 401)
+        done()
+      return this
+    },
+    send: function () {
+      return this
+    }
+  }
+  const next = function () {
+  }
+  m(req, res, next)
+})
+test('sets user=null for invalid userID', done => {
+  const userID = "invalid userID"
+  const req = {
+    session: {
+      passport: { user: userID }
+    }
+  }
+  const res = {}
+  const next = function () {
+    if (req.user === null)
+      done()
+  }
+  m(req, res, next)
+})
+
+test('returns user for valid userID - found in db', done => {
+  const userID = "01234567890123456789ABCD" // valid userID
+  const req = {
+    session: {
+      passport: { user: userID }
+    }
+  }
+  const res = {}
+  const next = function () {
+    expect(req.user).toBe(UserStub.user)
+    done()
+  }
+
+  UserStub.succeed = true
+  m(req, res, next)
+})
+test('returns user for valid userID - not found in db', done => {
+  const userID = "01234567890123456789ABCD" // valid userID
+  const req = {
+    session: {
+      passport: { user: userID }
+    }
+  }
+  const res = {}
+  const next = function () {
+    expect(req.user).toBe(null)
+    done()
+  }
+
+  UserStub.succeed = false
+  m(req, res, next)
+})
+
