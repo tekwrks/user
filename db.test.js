ee44ead4bee59d86bdbf13b7ba0339b16327921f
WhoMeNope <whomenope@outlook.com>
2018-12-02 19:14:32 +0100

    connection close test

--- db.test.js
+++ db.test.js
@@ -9,6 +9,10 @@ process.env.DATABASE_NAME = 'name'
 
 const uri = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_ADDRESS}/${process.env.DATABASE_NAME}`
 
+beforeEach(() => {
+  jest.resetModules()
+})
+
 test('creates connection with right uri', done => {
   jest.setMock('mongoose', {
     createConnection: function (u, os) {
@@ -60,6 +64,37 @@ test('retries on connection error', () => {
   }
 
   jest.setMock('mongoose', { createConnection })
+  require('./db')(uri, process.env.DATABASE_NAME)
+})
+
+test('closes connection on SIGINT', done => {
+  const mockClose = jest.fn()
+  const createConnection = function (u, os) {
+    return {
+      on: jest.fn(),
+      once: jest.fn(),
+      close: mockClose,
+    }
+  }
+  jest
+    .setMock('mongoose', { createConnection })
+
+  const exit = jest.spyOn(process, 'exit').mockImplementation(c => c)
+
+  jest
+    .spyOn(process, 'on')
+    .mockImplementation((signal, cb) => {
+      expect(signal).toBe('SIGINT')
+      mockClose.mockImplementation((cb2) => {
+        jest.spyOn(process, 'exit').mockImplementation((code) => {
+          expect(code).toBe(0)
+          done()
+        })
+        cb2()
+      })
+      cb()
+    })
+
   require('./db')(uri, process.env.DATABASE_NAME)
 })
 
